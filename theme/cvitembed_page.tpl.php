0462b4757560d70752858a6ce2100cb6a6f221bf
Lacey Sanderson <laceysanderson@781094.no-reply.drupal.org>
2017-03-09 16:56:19 -0600

    Switched so that each cvit diagram has it's own page to simplify the interface.

--- theme/cvitembed_page.tpl.php
+++ theme/cvitembed_page.tpl.php
@@ -4,44 +4,15 @@
  * Master template file of cvitembed module.
  */
 
- // Template structure:
- // <div></div>     - Active plot and plot selector.
- //
- // <div>
- //   <div></div>   - Left column - CVIT plot.
- //   <div></div>   - Right column - Legend and options.
- //   <div></div>
- // </div>
-
  // Current/active plot title.
  $active_plot = $form['active_plot']['#value'];
 ?>
-<div id="select-plots">
-  <span class="title active-plot"><?php print $active_plot; ?></span>
-  <span class="show-plots-link"><a href="#" id="link-available-plots">Available plots</a></span>
 
-  <div id="div-available-plots">
-    <ul>
-      <?php
-        // Construct list of available plots.
-        $plots = $form['arr_plots_available']['#value'];
-        foreach($plots as $k => $p) {
-          if ($p['title'] != $active_plot) {
-            // Exclude from the list the active/current plot.
-            $loc = '?data=' . $k;
 
-            print '<li>' . "\n";
-            print   '<a href="' . $loc . '">' . $p['title'] . '</a>' . "\n";
-            print   '<a href="' . $loc . '" class="btn btn-success" style="float:right; margin-right:10px;" alt="Show ' . $p['title'] . '" title="Show ' . $p['title'] . '">Show</a>' . "\n";
-            print "</li>\n";
-          }
-        }
-      ?>
-    </ul>
-  </div>
-</div>
+<div id="div-chart-container" width="1000" class="clearfix">
 
-<div id="div-chart-container">
+
+  <h1 class="title active-plot"><?php print $active_plot; ?></h1>
 
   <?php
     // Render CViT canvas.
